45ca517c3afe750efd3dfb3053e81d021b21bd2e
Jonatan <jonatanly@gmail.com>
2017-04-27 22:00:35 +0300

    Changed name of html
    Just trying stuff

--- gameworld.js
+++ gameworld.js
@@ -1,125 +1,54 @@
-/*var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render });
-
-function preload() {
-
-    game.load.tilemap('mario', 'sunjumpers.json', null, Phaser.Tilemap.TILED_JSON);
-    game.load.image('tiles', 'characte_right.png');
-    game.load.image('player', 'character_left.png');
-
-}
-
-var map;
-var tileset;
-var layer;
-var p;
-var cursors;
-
-function create() {
-
-    game.physics.startSystem(Phaser.Physics.ARCADE);
-
-    game.stage.backgroundColor = '#787878';
-
-    map = game.add.tilemap('mario');
-
-    map.addTilesetImage('SuperMarioBros-World1-1', 'tiles');
-
-    //  14 = ? block
-    // map.setCollisionBetween(14, 15);
-
-    map.setCollisionBetween(15, 16);
-    map.setCollisionBetween(20, 25);
-    map.setCollisionBetween(27, 29);
-    map.setCollision(40);
-    
-    layer = map.createLayer('World1');
-
-    layer.scale.set(0.5);
-
-    //  Un-comment this on to see the collision tiles
-    // layer.debug = true;
-
-    layer.resizeWorld();
-
-    p = game.add.sprite(32, 32, 'player');
-
-    game.physics.enable(p);
-
-    game.physics.arcade.gravity.y = 250;
-
-    p.body.bounce.y = 0.2;
-    p.body.linearDamping = 1;
-    p.body.collideWorldBounds = true;
-
-    game.camera.follow(p);
-
-    cursors = game.input.keyboard.createCursorKeys();
-
-}
-
-function update() {
-
-    game.physics.arcade.collide(p, layer);
-
-    p.body.velocity.x = 0;
-
-    if (cursors.up.isDown)
-    {
-        if (p.body.onFloor())
-        {
-            p.body.velocity.y = -200;
-        }
-    }
-
-    if (cursors.left.isDown)
-    {
-        p.body.velocity.x = -150;
-    }
-    else if (cursors.right.isDown)
-    {
-        p.body.velocity.x = 150;
-    }
-
-}
-
-function render() {
-
-    // game.debug.body(p);
-    game.debug.bodyInfo(p, 32, 320);
-
-}*/
-
 var game = new Phaser.Game(500, 400, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render });
 
 function preload() {
 
+    game.load.tilemap('level', 'sunjumpers.json', null, Phaser.Tilemap.TILED_JSON);
     game.load.image('playerRight', 'characte_right.png');
     game.load.image('playerLeft', 'character_left.png');
+    game.load.image('background', 'http://www.planwallpaper.com/static/images/cool-background.jpg')
 
 }
 
 var p;
+var map;
+var layer;
+var bg;
+var cursors;
 
 function create() {
   game.physics.startSystem(Phaser.Physics.ARCADE);
 
     game.stage.backgroundColor = '#787878';
     
-    p = game.add.sprite(40, 200, 'playerLeft');
+    bg = game.add.tileSprite(0, 0, 800, 600,('background'));
+    p = game.add.sprite(40, 200, 'playerRight');
+    bg.fixedToCamera = true;
     
+    //  Un-comment this on to see the collision tiles
+    // layer.debug = true;
     
-    game.physics.enable(p);
+    //map = game.add.tilemap('level');
+    //map.addTilesetImage('playerLeft');
+    //map.setCollisionByExclusion([ 13, 14, 15, 16, 46, 47, 48, 49, 50, 51 ]);
+    
+    //layer = map.createLayer('Tile Layer 1');
+    //layer.resizeWorld();
+    
+    game.physics.enable(p, Phaser.Physics.ARCADE);
 
     game.physics.arcade.gravity.y = 250;
 
     p.body.bounce.y = 0.2;
     p.body.linearDamping = 1;
     p.body.collideWorldBounds = true;
+    
     game.camera.follow(p);
     cursors = game.input.keyboard.createCursorKeys();
 }
 
 function update() {
+    
+    //game.physics.arcade.collide(player, layer);
 
     p.body.velocity.x = 0;
         if (p.body.onFloor())
