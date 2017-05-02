f04421e3bfb917ef6cacc6a374300a7117fda4fc
Jonatan <jonatanly@gmail.com>
2017-05-02 16:19:07 +0300

    Changed everything

--- gameworld.js
+++ gameworld.js
@@ -1,73 +1,154 @@
-var game = new Phaser.Game(500, 400, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render });
+var Jumper = function() {};
+Jumper.Play = function() {};
 
-function preload() {
+Jumper.Play.prototype = {
 
-    //game.load.tilemap('level', 'sunjumpers.json', null, Phaser.Tilemap.TILED_JSON);
-    game.load.image('playerRight', 'characte_right.png');
-    game.load.image('playerLeft', 'character_left.png');
-    //game.load.image('background', 'http://www.planwallpaper.com/static/images/cool-background.jpg')
-    game.load.image('wall', 'box.png.png')
-
-}
-
-var p;
-var map;
-var layer;
-var bg;
-var cursors;
-var wall;
-
-function create() {
-  game.physics.startSystem(Phaser.Physics.ARCADE);
-
-    game.stage.backgroundColor = '#787878';
+  preload: function() {
+    this.load.image( 'playerRight', 'rsz_character_right.png' );
+    this.load.image( 'playerLeft', 'rsz_character_left.png' );
+    this.load.image( 'pixel', 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/836/pixel_1.png' );
+    this.load.image('background', 'http://clipartix.com/wp-content/uploads/2016/08/Sunrise-clipart-2.jpg')
+  },
     
-    //bg = game.add.tileSprite(0, 0, 800, 600,('background'));
-    p = game.add.sprite(40, 200, 'playerRight');
-    wall = game.add.sprite(400, 350, 'wall');
-    //bg.fixedToCamera = true;
+  create: function() {
+    // background picture
+    var bg = game.add.tileSprite(0, 0, 300, 600,('background'));
+    bg.fixedToCamera = true;
     
-    //  Un-comment this on to see the collision tiles
-    // layer.debug = true;
-    
-    game.physics.enable(p, Phaser.Physics.ARCADE);
-    wall.name = 'wall';
-    game.physics.enable(wall, Phaser.Physics.ARCADE);
-    
-    wall.body.immovable = true;
-    
-    p.body.gravity.y = 250;
-    p.body.bounce.y = 0.2;
-    p.body.linearDamping = 1;
-    p.body.collideWorldBounds = true;
-    
-    game.camera.follow(p);
-    cursors = game.input.keyboard.createCursorKeys();
-}
+      // scaling
+//    this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+//    this.scale.maxWidth = this.game.width;
+//    this.scale.maxHeight = this.game.height;
+//    this.scale.pageAlignHorizontally = true;
+//    this.scale.pageAlignVertically = true;
 
-function update() {
-    
-    game.physics.arcade.collide(p, wall);
+    // physics
+    this.physics.startSystem( Phaser.Physics.ARCADE );
 
-    p.body.velocity.x = 0;
-        if (p.body.onFloor())
-        {
-            p.body.velocity.y = -200;
-        }
+    // camera and platform tracking vars
+    this.cameraYMin = 99999;
+    this.platformYMin = 99999;
 
-    if (cursors.left.isDown)
-    {
-        p.body.velocity.x = -150;
-        p.loadTexture('playerLeft', 0, false);
+    // create platforms
+    this.platformsCreate();
+
+    // create hero
+    this.heroCreate();
+
+    // cursor controls
+    this.cursor = this.input.keyboard.createCursorKeys();
+  },
+
+  update: function() {
+    // this is where the main magic happens
+    // the y offset and the height of the world are adjusted
+    // to match the highest point the hero has reached
+    this.world.setBounds( 0, -this.hero.yChange, this.world.width, this.game.height + this.hero.yChange );
+
+    // the built in camera follow methods won't work for our needs
+    // this is a custom follow style that will not ever move down, it only moves up
+    this.cameraYMin = Math.min( this.cameraYMin, this.hero.y - this.game.height + 130 );
+    this.camera.y = this.cameraYMin;
+
+    // hero collisions and movement
+    this.physics.arcade.collide( this.hero, this.platforms );
+    this.heroMove();
+
+    // for each plat form, find out which is the highest
+    // if one goes below the camera view, then create a new one at a distance from the highest one
+    // these are pooled so they are very performant
+      
+    this.platforms.forEachAlive( function( elem ) {
+      this.platformYMin = Math.min( this.platformYMin, elem.y );
+      if( elem.y > this.camera.y + this.game.height ) {
+        elem.kill();
+        this.platformsCreateOne( this.rnd.integerInRange( 0, this.world.width - 50 ), this.platformYMin - 100, 50 );
+      }
+    }, this );
+  },
+
+  shutdown: function() {
+    // reset everything, or the world will be messed up
+    this.world.setBounds( 0, 0, this.game.width, this.game.height );
+    this.cursor = null;
+    this.hero.destroy();
+    this.hero = null;
+    this.platforms.destroy();
+    this.platforms = null;
+  },
+
+  platformsCreate: function() {
+    // platform basic setup
+    this.platforms = this.add.group();
+    this.platforms.enableBody = true;
+    this.platforms.createMultiple( 100, 'pixel' );
+
+    // create the base platform, with buffer on either side so that the hero doesn't fall through
+    this.platformsCreateOne( -16, this.world.height - 16, this.world.width + 16 );
+    // create a batch of platforms that start to move up the level
+    for( var i = 0; i < 9; i++ ) {
+      this.platformsCreateOne( this.rnd.integerInRange( 0, this.world.width - 50 ), this.world.height - 100 - 100 * i, 50 );
     }
-    else if (cursors.right.isDown)
-    {
-        p.body.velocity.x = 150;
-        p.loadTexture('playerRight', 0, false);
+  },
+
+  platformsCreateOne: function( x, y, width ) {
+    // this is a helper function since writing all of this out can get verbose elsewhere
+    var platform = this.platforms.getFirstDead();
+    platform.reset( x, y );
+    platform.scale.x = width;
+    platform.scale.y = 16;
+    platform.body.immovable = true;
+    return platform;
+  },
+
+  heroCreate: function() {
+    // basic hero setup
+    this.hero = game.add.sprite( this.world.centerX, this.world.height - 36, 'playerRight' );
+    this.hero.anchor.set( 0.5 );
+    
+    // track where the hero started and how much the distance has changed from that point
+    this.hero.yOrig = this.hero.y;
+    this.hero.yChange = 0;
+
+    // hero collision setup
+    // disable all collisions except for down
+    this.physics.arcade.enable( this.hero );
+    this.hero.body.gravity.y = 500;
+    this.hero.body.checkCollision.up = false;
+    this.hero.body.checkCollision.left = false;
+    this.hero.body.checkCollision.right = false;
+  },
+
+  heroMove: function() {
+    // handle the left and right movement of the hero
+    if( this.cursor.left.isDown ) {
+      this.hero.body.velocity.x = -200
+        this.hero.loadTexture('playerLeft', 0, false);
+    } else if( this.cursor.right.isDown ) {
+      this.hero.body.velocity.x = 200;
+        this.hero.loadTexture('playerRight', 0, false);
+    } else {
+      this.hero.body.velocity.x = 0;
     }
 
+    // handle hero jumping
+    if(this.hero.body.touching.down ) {
+      this.hero.body.velocity.y = -350;
+    } 
+    
+    // wrap world coordinated so that you can warp from left to right and right to left
+    this.world.wrap( this.hero, this.hero.width / 2, false );
+
+    // track the maximum amount that the hero has travelled
+    this.hero.yChange = Math.max( this.hero.yChange, Math.abs( this.hero.y - this.hero.yOrig ) );
+    
+    // if the hero falls below the camera view, gameover
+    if( this.hero.y > this.cameraYMin + this.game.height && this.hero.alive ) {
+      this.state.start( 'Play' );
+    }
+  }
 }
 
-function render() {
-    
-}
+var game = new Phaser.Game( 300, 500, Phaser.CANVAS, '' );
+game.state.add( 'Play', Jumper.Play );
+game.state.start( 'Play' );
