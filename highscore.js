1b36f6b0b4a56bd656f8ea7bd74751b129a1f4b7
ellenaar <ellenaarinen@Ellen-MacBook-Pro.local>
2017-05-09 22:06:17 +0300

    new fonts

--- highscore.js
+++ highscore.js
@@ -1,9 +1,61 @@
 var Highscore = function() {};
 
+WebFontConfig = {
+
+    //  'active' means all requested fonts have finished loading
+    //  We set a 1 second delay before calling 'createText'.
+    //  For some reason if we don't the browser cannot render the text the first time it's created.
+    active: function() { game.time.events.add(Phaser.Timer.SECOND, createText, this); },
+
+    //  The Google Fonts we want to load (specify as many as you like in the array)
+    google: {
+      families: ['Revalia']
+    }
+
+};
+
+function createText(){
+    text = game.add.text(game.world.centerX, 100, "Game Over\n ");
+    text.anchor.setTo(0.5);
+
+    text.font = 'Revalia';
+    text.fontSize = 40;
+
+    //  x0, y0 - x1, y1
+    grd = text.context.createLinearGradient(0, 0, 0, text.canvas.height);
+    grd.addColorStop(0, '#8ED6FF');   
+    grd.addColorStop(1, '#004CB3');
+    text.fill = grd;
+
+    text.align = 'center';
+    text.stroke = '#000000';
+    text.strokeThickness = 2;
+    text.setShadow(5, 5, 'rgba(0,0,0,0.5)', 5);
+
+    text.inputEnabled = true;
+    text.input.enableDrag();
+
+    text.events.onInputOver.add(over, this);
+    text.events.onInputOut.add(out, this);
+};
+
+function out() {
+
+    text.fill = grd;
+
+};
+
+function over() {
+
+    text.fill = '#006600';
+
+};
+
 Highscore.prototype = {
     
     preload: function() {
         this.load.image('background', 'Sunrise-clipart-2.jpg');
+          game.load.script('webfont', '//ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js');
     },
     
     init: function () {
@@ -15,9 +67,10 @@ Highscore.prototype = {
         this.titleText.setShadow(3, 3, 'rgba(0,0,0,0.5)', 5);
         this.titleText.anchor.set(0.5);
         this.optionCount = 1;
+        this.titleText.font = 'Revalia';
         
-        this.instruText = game.make.text(game.world.centerX, 250, "Your score: " + scoreText + "    Your Highscore: " + localStorage.getItem("Highscore") , {
-          font: '16pt TheMinion',
+        this.instruText = game.make.text(game.world.centerX, 250, "Your score: " + scoreText + "\nYour Highscore: " + localStorage.getItem("Highscore") , {
+          font: '16pt Revalia',
           fill: '#00000',
             align: 'center',
             wordWrap: true,
